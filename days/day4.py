c98ab3a505bedc5c89a310cd9f47ee90e787e05d
Aggrobane <william-fecteau@hotmail.com>
2020-12-04 21:49:36 -0500

    Refactor Day 3

--- days/day4.py
+++ days/day4.py
@@ -4,7 +4,6 @@ import re
 @day(4)
 class Day4(AOCDay):
     def common(self):
-        validKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
         passports = []
         strPassport = ""
 
@@ -12,90 +11,84 @@ class Day4(AOCDay):
             if line != "":
                 strPassport += line + " "
             else:
-                dicPassport = {}
-                values = strPassport.split(' ')
-
-                for value in values:
-                    if(value != ""):
-                        key, val = value.split(':')
-                        if key in validKeys:
-                            dicPassport[key] = val
-                passports.append(dicPassport)
+                keyVal = strPassport.split(' ')
+                dic = self.convertKeyValToDic(keyVal)
+                
+                # S'il est valide
+                if len(dic) == 7:
+                    passports.append(dic)
                 strPassport = ""
 
-        if(strPassport != ""):
-            for value in values:
-                if(value != ""):
-                    key, val = value.split(':')
-                    if key in validKeys:
-                        dicPassport[key] = val
-            passports.append(dicPassport)
-            strPassport = ""
-
+        # Pour la dernière ligne, qui n'est pas séparé par un ""    
+        keyVal = strPassport.split(' ')
+        passports.append(self.convertKeyValToDic(keyVal))
+            
         self.inputData = passports
 
+    def convertKeyValToDic(self, values):
+        dicPassport = {}
+        for value in values:
+            if(value != ""):
+                key, val = value.split(':')
+
+                # On ignore "cid"
+                if key != "cid":
+                    dicPassport[key] = val
+
+        return dicPassport
 
     def part1(self):
-        valid = 0
-
-        for passport in self.inputData:
-            if(len(passport) == 7):
-                valid += 1
-
-
-        return valid
+        return len(self.inputData)
     
-    #155 too high
+
     def part2(self):
-        cpt = 0
+        validCpt = 0
 
         for passport in self.inputData:
-            if(len(passport) == 7):
-                valid = True
+            valid = True
 
-                validEcl = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-                for key in passport:
+            validEcl = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
-                    
-                    val = passport[key]
+            for key in passport:
+                val = passport[key]
 
-                    if key == "byr":
-                        if len(val) != 4:
+                if key == "byr":
+                    if len(val) != 4:
+                        valid = False
+                    elif int(val) < 1920 or int(val) > 2002:
+                        valid = False
+                elif key == "iyr":
+                    if len(val) != 4:
+                        valid = False
+                    elif int(val) < 2010 or int(val) > 2020:
+                        valid = False
+                elif key == "eyr":
+                    if len(val) != 4:
+                        valid = False
+                    elif int(val) < 2020 or int(val) > 2030:
+                        valid = False
+                elif key == "hgt":
+                    if "cm" in val:
+                        val = val[:-2]
+                        if int(val) < 150 or int(val) > 193:
                             valid = False
-                        elif int(val) < 1920 or int(val) > 2002:
+                    elif "in" in val:
+                        val = val[:-2]
+                        if int(val) < 59 or int(val) > 76:
                             valid = False
-                    elif key == "iyr":
-                        if len(val) != 4:
-                            valid = False
-                        elif int(val) < 2010 or int(val) > 2020:
-                            valid = False
-                    elif key == "eyr":
-                        if len(val) != 4:
-                            valid = False
-                        elif int(val) < 2020 or int(val) > 2030:
-                            valid = False
-                    elif key == "hgt":
-                        if "cm" in val:
-                            val = val[:-2]
-                            if int(val) < 150 or int(val) > 193:
-                                valid = False
-                        elif "in" in val:
-                            val = val[:-2]
-                            if int(val) < 59 or int(val) > 76:
-                                valid = False
-                        else:
-                            valid = False
-                    elif key == "hcl":
-                        if not re.search("^#[0-9a-f]{6}$", val):
-                            valid = False
-                    elif key == "ecl":
-                        if val not in validEcl:
-                            valid = False
-                    elif key == "pid":
-                        if not re.search("^[0-9]{9}$", val):
-                            valid = False
+                    else:
+                        valid = False
+                elif key == "hcl":
+                    if not re.search("^#[0-9a-f]{6}$", val):
+                        valid = False
+                elif key == "ecl":
+                    if val not in validEcl:
+                        valid = False
+                elif key == "pid":
+                    if not re.search("^[0-9]{9}$", val):
+                        valid = False
 
-                if valid:
-                    cpt += 1
+            if valid:
+                validCpt += 1
 
-        return cpt
+        return validCpt
